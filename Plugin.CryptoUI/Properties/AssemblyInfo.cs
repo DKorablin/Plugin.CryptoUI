f2e957bfb9ec90d0250102908e55bf6480fead30
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:53:02 +0300

    Unified plugins deployment

--- Plugin.CryptoUI/Properties/AssemblyInfo.cs
+++ Plugin.CryptoUI/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("0d31a0ae-154e-4208-8e48-be90e8a22f69")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,16 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.CryptoUI")]
 #else
 
-[assembly: AssemblyTitle("Plugin.CryptoUI")]
 [assembly: AssemblyDescription("Certificate Generator/Splitter")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.CryptoUI")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2018-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2018-2025")]
 #endif
 
 /*if $(ConfigurationName) == Release (
